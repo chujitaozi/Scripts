1595ad18c932f7cfa07b8874f6eb49cfd21f000e
chujitaozi <1484548330@qq.com>
2018-01-28 11:29:02 +0800

    add scripts for handsfree

---
+++ ros/indigo_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install the ros-indigo
+echo 加入国内源
+sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.ustc.edu.cn/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+echo \\n
+sudo apt-get update
+sudo apt-get -y install ros-indigo-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get -y install python-rosinstall
+
+echo indigo已完成安装，请关闭此终端
+
