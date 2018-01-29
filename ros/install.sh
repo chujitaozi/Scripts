48d96da0de2a10679ec59f8fb76e3fcf42cb794f
chujitaozi <1484548330@qq.com>
2018-01-29 16:20:42 +0800

    add rbx1/2 in handsfree

--- ros/install.sh
+++ ros/install.sh
@@ -8,6 +8,8 @@ mkdir -p /opt/ros_handsfree/src
 git clone https://github.com/HANDS-FREE/handsfree.git /opt/ros_handsfree/src
 cd /opt/ros_handsfree/src/Documentation
 sh environment_config.sh -y -y
+sh rbx1-prereq.sh
+sh rbx2-prereq.sh
 cd ../handsfree_hw/src/
 find -name 'main.cpp' | xargs perl -pi -e 's|/home/kedou/ros_workspace/mobile_robot_ws/src/handsfree/handsfree_hw|/opt/ros_handsfree/src/handsfree_hw|g'
 cd /opt/ros_handsfree/
